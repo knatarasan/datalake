b163163b613d167cfc420d050192cd39dfd34376
knatarasan <kannappan_natarasan@yahoo.com>
2015-11-01 19:58:19 -0800

    fixed a bug on update

--- dw/ingest/em_mara_scd2.sql
+++ dw/ingest/em_mara_scd2.sql
@@ -99,6 +99,7 @@ where item.eff_to_date='2999-12-31';
 
 update dw_sourcing.d_Item item join em_erp.em_mara_changed em_mara_changed
 on item.material_number =em_mara_changed.MATNR
+and item.eff_to_date='2999-12-31'
  set eff_to_date='2015-10-11';
 
 #-- 4.2 Insert new rows
